316e4b2f013826b1cad5cc566859e338a5021011
ripan <er.ripankumar@gmail.com>
2014-05-11 00:41:10 +0100

    fix css

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,5 +1,5 @@
 class Identity < OmniAuth::Identity::Models::ActiveRecord
-  validates :email,:name, :presence => {:message => "Required" }
+  validates :name,:email, :presence => {:message => "Required" }
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i, :allow_blank => true, :multiline => true 
   validates :email, :uniqueness =>  true
 end
