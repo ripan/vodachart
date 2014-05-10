c975e462ff465742d83637f51144f83ccd139ab2
ripan <er.ripankumar@gmail.com>
2014-05-10 16:45:15 +0100

    app setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vodachart::Application.config.secret_key_base = 'f4fe0fbff1d002fa21e9f9f4eeef99e8f3bada9c1bb98a75eb6130e8e15b6ed769f54239b7bc462ecfcc9bf90590a1e60921be13bddacef7ba55b4d429b6b143'
