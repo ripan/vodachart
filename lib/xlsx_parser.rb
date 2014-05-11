06fe93bcf14a1dce9b403e8a5cbe28c8316c9c8a
ripan <er.ripankumar@gmail.com>
2014-05-12 00:53:27 +0100

    Refactor

--- lib/xlsx_parser.rb
+++ lib/xlsx_parser.rb
@@ -2,13 +2,14 @@ require 'rubygems'
 require 'simple_xlsx_reader'
 
 class XlsxParser
-  attr_accessor :rows, :header, :galaxies, :planets, :alien_categories
+  attr_accessor :rows, :header, :galaxies, :planets, :aliens, :alien_categories
   def initialize(filename)
     doc = SimpleXlsxReader.open(filename)
     @rows = doc.sheets.first.rows
     @header = @rows[0]
     @galaxies = []
     @planets = []
+    @aliens = []
     @alien_categories = []
 
     resources = {0 => "Alien", 1=>"AlienCategory", 2 => "Planet", 3 => "Galaxy"}
@@ -16,6 +17,7 @@ class XlsxParser
     @rows.drop(1).each_with_index do |row, index|
       #puts resource = resources[index].constantize
       puts "#{row[0]} #{row[1]} #{row[2]} #{row[3]}"
+      @aliens << row[0]
       @alien_categories << row[1]
       @planets << row[2]
       @galaxies << row[3]
@@ -36,5 +38,7 @@ puts "\n\ngalaxies \n\n"
 puts parser.galaxies
 puts "\n\nplanets \n\n"
 puts parser.planets
+puts "\n\naliens \n\n"
+puts parser.aliens
 puts "\n\nalien_categories \n\n"
 puts parser.alien_categories
