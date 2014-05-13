3d7aa072e7843b375cdfc8fdf121bac15e4b2ddb
ripan <er.ripankumar@gmail.com>
2014-05-13 23:23:41 +0100

    Refactor

--- lib/xlsx_parser.rb
+++ lib/xlsx_parser.rb
@@ -15,12 +15,10 @@ class XlsxParser
     @product_families = []
     @stages = []
     @orders = []
+  end
 
-    @resource_mapping = {0 => "Alien", 1=>"AlienCategory", 2 => "Planet", 3 => "Galaxy", 4 => "Product", 5 => "ProductFamily", 6 => "Stage"}
-
-    @rows.drop(1).each_with_index do |row, index|
-      #puts resource = resources[index].constantize
-      #puts "#{row[0]} #{row[1]} #{row[2]} #{row[3]}"
+  def parse_data
+     @rows.drop(1).each_with_index do |row, index|
 
       alien = row[0]
       alien_category = row[1]
@@ -35,53 +33,64 @@ class XlsxParser
       setup_charge = row[10]
       monthly_revenue = row[11]
 
+      
 
-      @aliens << {:name => alien, :alien_category => alien_category, :planet => planet}
-      @alien_categories << {:name => alien_category}
-      @planets << {:name => planet, :galaxy => galaxy}
-      @galaxies << {:name => galaxy}
-      @products << {:name => product, :product_family => product_family}
-      @product_families << {:name => product_family}
-      @stages << {:name => stage}
-      @orders << {:alien => alien, :product => product, :stage => stage, :created_at => created_at, :closed_at => closed_at, :is_closed => is_closed, :setup_charge => setup_charge, :monthly_revenue => monthly_revenue}
+      h_alien = {:name => alien, :alien_category => alien_category, :planet => planet}
+      h_alien_category = {:name => alien_category}
+      h_planet = {:name => planet, :galaxy => galaxy}
+      h_galaxy = {:name => galaxy}
+      h_product = {:name => product, :product_family => product_family}
+      h_product_family = {:name => product_family}
+      h_stage =  {:name => stage}
+      h_order = {:alien => alien, :product => product, :stage => stage, :created_at => created_at, :closed_at => closed_at, :is_closed => is_closed, :setup_charge => setup_charge, :monthly_revenue => monthly_revenue}
+
+
+      aliens << h_alien unless aliens.include? h_alien
+      alien_categories << h_alien_category unless alien_categories.include? h_alien_category
+      planets << h_planet unless planets.include? h_planet
+      galaxies << h_galaxy unless galaxies.include? h_galaxy
+      products << h_product unless products.include? h_product
+      product_families << h_product_family unless product_families.include? h_product_family
+      stages << h_stage unless stages.include? h_stage
+      orders << h_order #unless orders.include? h_order
     end
   end
 
-  def create_tables
+  def import_data
     puts "Create Galaxies"
-    @galaxies.uniq.each do  |galaxy|
+    galaxies.each do  |galaxy|
       Galaxy.create(galaxy)
     end
     puts "Create Alien Categories"
-    @alien_categories.uniq.each do  |alien_category|
+    alien_categories.each do  |alien_category|
       AlienCategory.create(alien_category)
     end
     puts "Create Product Families"
-    @product_families.uniq.each do  |product_family|
+    product_families.each do  |product_family|
       ProductFamily.create(product_family)
     end
     puts "Create Stages"
-    @stages.uniq.each do  |stage|
+    stages.each do  |stage|
       Stage.create(stage)
     end
     puts "Create Planets"
-    @planets.uniq.each do  |planet|
+    planets.each do  |planet|
       planet[:galaxy] = Galaxy.find_by(:name => planet[:galaxy])
       Planet.create(planet)
     end
     puts "Create Aliens"
-    @aliens.uniq.each do  |alien|
+    aliens.each do  |alien|
       alien[:planet] = Planet.find_by(:name => alien[:planet])
       alien[:alien_category] = AlienCategory.find_by(:name => alien[:alien_category])
       Alien.create(alien)
     end
     puts "Create Products"
-    @products.uniq.each do  |product|
+    products.each do  |product|
       product[:product_family] = ProductFamily.find_by(:name => product[:product_family])
       Product.create(product)
     end
     puts "Create Orders"
-    @orders.each do  |order|
+    orders.each do  |order|
       order[:alien] = Alien.find_by(:name => order[:alien])
       order[:product] = Product.find_by(:name => order[:product])
       order[:stage] = Stage.find_by(:name => order[:stage])
@@ -93,25 +102,31 @@ end
 
 file_name = "TestDataSmall.xlsx"
 parser = XlsxParser.new(file_name)
-#parser.rows.count
-#parser.header
+parser.parse_data
+parser.import_data
 
-puts "\n\ngalaxies \n\n"
-puts parser.galaxies.uniq.count
+puts "\n\ngalaxies"
+puts parser.galaxies.count
 
-puts "\n\nplanets \n\n"
-puts parser.planets.uniq.count
+puts "\n\nplanets"
+puts parser.planets.count
 
-puts "\n\naliens \n\n"
-puts parser.aliens.uniq.count
+puts "\n\naliens"
+puts parser.aliens.count
 
-puts "\n\nproducts \n\n"
-puts parser.products.uniq.count
+puts "\n\nalien categories"
+puts parser.alien_categories.count
 
-puts "\n\nstages \n\n"
-puts parser.stages.uniq.count
+puts "\n\nproducts"
+puts parser.products.count
 
-puts "\n\orders \n\n"
+puts "\n\nproduct families"
+puts parser.product_families.count
+
+puts "\n\nstages"
+puts parser.stages.count
+
+puts "\n\orders"
 puts parser.orders.count
 
-parser.create_tables
+
