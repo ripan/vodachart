a2f3dd0abd41a36b9fdccaa3979061fe1a58d1d6
ripan <er.ripankumar@gmail.com>
2014-05-14 07:33:24 +0100

    Add require login filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
   end
 
   private
+
+  def require_login
+    unless signed_in?
+      #flash[:error] = "You must be logged in to access this section"
+      redirect_to :signin
+    end
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
